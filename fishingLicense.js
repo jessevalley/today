25fd702b94dd06a881cb0820b1696e5dadc22aea
Valley <valjesc@dunwoody.edu>
2016-12-08 09:46:31 -0600

    Initial commit

---
+++ fishingLicense.js
@@ -0,0 +1,650 @@
+var data = [
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 3570
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 4144
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 4312
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 4557
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 4452
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 3815
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 35
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 63
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 28
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 42
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 42
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 42
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 714
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 721
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 805
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 826
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 700
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 588
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 497
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 441
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 294
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 434
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 217
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 280
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 22211
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 23219
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 18942
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 23100
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 20538
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 18900
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 28
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 2842
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 2800
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 2492
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 2856
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 2457
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 2408
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 7
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 1813
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 1967
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 2114
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 2079
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 2142
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 2023
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 14
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 1498
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 1862
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 1295
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 1456
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 1505
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 1799
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "HENNEPIN",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 13216
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "HENNEPIN",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 13342
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "HENNEPIN",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 12929
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "HENNEPIN",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 13160
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "HENNEPIN",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 11445
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 11403
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "HENNEPIN",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 14
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 14
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 28
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 42
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "INLAND TROUT STAMP",
+    "NUMBER_SOLD": 7
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 14
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 7
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 7
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "LAKE SALMON FISHING",
+    "NUMBER_SOLD": 7
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 1568
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 1624
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 1281
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 1337
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 1232
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 672
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 287
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 259
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 168
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 252
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 126
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "NORTHERN FISHING",
+    "NUMBER_SOLD": 63
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 4228
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 4312
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 4207
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 4508
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 3913
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "PIKE FISHING",
+    "NUMBER_SOLD": 2814
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 791
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 882
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 819
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 833
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 658
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "SUNNY FISHING",
+    "NUMBER_SOLD": 469
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 441
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 469
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 469
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 413
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 441
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "CATFISH FISHING",
+    "NUMBER_SOLD": 371
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 364
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 777
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 392
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 483
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 868
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "BIG MOUTH BASS FISHING",
+    "NUMBER_SOLD": 329
+  },
+  {
+    "TXNL_LICN_YEAR": 2006,
+    "COUNTY": "RAMSEY",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 1876
+  },
+  {
+    "TXNL_LICN_YEAR": 2007,
+    "COUNTY": "RAMSEY",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 1715
+  },
+  {
+    "TXNL_LICN_YEAR": 2008,
+    "COUNTY": "RAMSEY",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 1722
+  },
+  {
+    "TXNL_LICN_YEAR": 2009,
+    "COUNTY": "RAMSEY",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 1722
+  },
+  {
+    "TXNL_LICN_YEAR": 2010,
+    "COUNTY": "RAMSEY",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 1547
+  },
+  {
+    "TXNL_LICN_YEAR": 2011,
+    "COUNTY": "RAMSEY",
+    "License": "WALLEYE FISHING",
+    "NUMBER_SOLD": 1232
+  },
+]
